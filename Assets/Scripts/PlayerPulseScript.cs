756f873b25c36ad816d079a6bae43f2d1990238e
GodScrub <david_avila_bhc@yahoo.com>
2017-01-21 15:12:47 -0800

    Added SFX, Camera Shake

--- Assets/Scripts/PlayerPulseScript.cs
+++ Assets/Scripts/PlayerPulseScript.cs
@@ -2,11 +2,18 @@
 using System.Collections;
 
 public class PlayerPulseScript : MonoBehaviour {
-
+    public float camShakeDampFactor = .25f;
+    public float coolDownTime;
 	public float _pulseDist;
 	public float MaxCharge;
-	public float chargeSpeed;
+
+    public float chargeRate = 10.0f;
+    public float maxCharge = 100.0f;
+    public float currentCharge = 0.0f;
+    public float drainRate = 10.0f;
 	private float _sonarCharge;
+    private bool canCharge = true;
+    private bool discharge = false;
 	// Use this for initialization
 	void Start () {
 		
@@ -14,29 +21,51 @@ public class PlayerPulseScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Space) && _sonarCharge < MaxCharge) {
-			_sonarCharge += Time.deltaTime * chargeSpeed;
-		
+		if (Input.GetKey (KeyCode.Space) && _sonarCharge < MaxCharge && canCharge) {
+			_sonarCharge += Time.deltaTime * chargeRate;
+            currentCharge = Mathf.Clamp(currentCharge + chargeRate * Time.deltaTime, 0, maxCharge);
+            if(currentCharge == maxCharge)
+            {
+                canCharge = false;
+            }
 		}
 		else if(Input.GetKeyUp(KeyCode.Space))
 		{
-			Debug.Log(_sonarCharge);
-			Collider[] walls = Physics.OverlapSphere (transform.position, 30.5f);
-			foreach (Collider c in walls) 
-			{
-				Debug.Log ("Hit");
-				WallMaterialScript scr = c.GetComponent<WallMaterialScript> ();
-				if (scr != null) {
-					scr.SetSonar (transform.position, 25);
-				}
-			}
-			_sonarCharge = 0;
+            releaseCharge();
 		}
 
+        if (discharge)
+        {
+            currentCharge = Mathf.Clamp(currentCharge - Time.deltaTime * drainRate, 0, maxCharge);
+            if (currentCharge == 0)
+            {
+                canCharge = true;
+                discharge = false;
+            }
+                
+        }    
 	}
 
 	public float ChargeAmount()
 	{
 		return _pulseDist;
 	}
+
+    public void releaseCharge()
+    {
+        StartCoroutine(Camera.main.GetComponent<CameraManager>().Shake((currentCharge / maxCharge) * camShakeDampFactor));
+        Debug.Log(_sonarCharge);
+        Collider[] walls = Physics.OverlapSphere(transform.position, 30.5f);
+        foreach (Collider c in walls)
+        {
+            Debug.Log("Hit");
+            WallMaterialScript scr = c.GetComponent<WallMaterialScript>();
+            if (scr != null)
+            {
+                scr.SetSonar(transform.position, 25);
+            }
+        }
+        _sonarCharge = 0;
+        discharge = true;
+    }
 }
