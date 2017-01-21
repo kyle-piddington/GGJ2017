04ed680c242673d3f5134b2a32f6a70352252a1d
Michael Casebolt <mikebolt@gmail.com>
2017-01-20 22:18:50 -0800

    Add a script for collectable beacons and add a game controller class.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	private static int numCollectedBeacons = 0;
+
+	public static void incrementCollectedBeacons() {
+
+		// TODO: trigger sound effects, for example
+
+		++numCollectedBeacons;
+	}
+
+	public static int getNumCollectedBeacons() {
+		return numCollectedBeacons;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
