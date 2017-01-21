dfc96e72e9ff70b73c857e4564cf2336f8d451f1
Kyle Piddington <kylepiddington@Kyles-MacBook-Pro.local>
2017-01-21 10:45:23 -0800

    Whoops forgot to track

---
+++ Assets/Scripts/WallMaterialScript.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallMaterialScript : MonoBehaviour {
+
+	public const float SonarSpeed = 30;
+	private GameObject _player;
+	private PlayerPulseScript _playerPulseScript;
+	private Renderer _renderer;
+	private float _highlightTime;
+	public float HighlightTime = 2.5f;
+	private Vector3 _sonarPosition;
+	private float _maxDist = 0;
+	private float sonarTime = 0;
+	// Use this for initialization
+	void Start () {
+		_renderer = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sonarTime += Time.deltaTime * SonarSpeed;
+
+		_renderer.material.SetVector("_PlayerPosition", _sonarPosition);
+		_renderer.material.SetFloat ("_VisibleDistance", sonarTime);
+		_renderer.material.SetFloat ("_MaxDistance", _maxDist);
+
+
+	}
+
+	public void SetSonar(Vector3 location, float maxDist){
+		_sonarPosition = location;
+		_maxDist = maxDist;
+		sonarTime = 0;
+	}
+
+
+		
+}
