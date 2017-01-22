16dc5557656b2e78cdc735d7af2c8282c382b64e
Kyle Piddington <kylepiddington@Kyles-MacBook-Pro.local>
2017-01-22 11:23:55 -0800

    Splash Screen

---
+++ Assets/SplashScreenTiming.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenTiming : MonoBehaviour {
+	public float fadeTime = 1;
+
+	IEnumerator runSplashScreen()
+	{
+		yield return new WaitForSeconds (fadeTime);
+		gameObject.GetComponent<Image>().CrossFadeAlpha(0, fadeTime, false);
+		yield return new WaitForSeconds (fadeTime);
+		SceneManager.LoadScene ("MainMenu");
+		yield return null;
+	}
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<CanvasRenderer>().SetAlpha( 0.0f );
+		gameObject.GetComponent<Image>().CrossFadeAlpha(1, fadeTime, false);
+		StartCoroutine (runSplashScreen());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
