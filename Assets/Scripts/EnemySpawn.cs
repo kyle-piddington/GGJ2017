0d56c2af2c896a22c995c9ec815d36b9092eb198
cchung89 <cchao06@calpoly.edu>
2017-01-22 14:15:35 -0800

    Enemy Spawning Location and Capability Added!!!

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+
+    public void Spawn()
+    {
+        if (GameManager.numCollectedBeacons == 1)
+        {
+            Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Spawn();
+	}
+}
