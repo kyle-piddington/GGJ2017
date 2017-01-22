229dea107d9e86c2a27bc59385b25c4f1d9bb363
GodScrub <david_avila_bhc@yahoo.com>
2017-01-22 12:36:02 -0800

    Added sfx to statues

--- Assets/Scripts/BeaconCollection.cs
+++ Assets/Scripts/BeaconCollection.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BeaconCollection : MonoBehaviour {
-
-	private const double BEACON_COLLECTION_DISTANCE = 0.5;
+    public AudioSource beaconAhoy;
+    public AudioSource beaconPickUp;
+	private const float BEACON_COLLECTION_DISTANCE = 0.5f;
 
 	private GameObject beacon;
 	private GameObject player;
 
 	// Use this for initialization
 	void Start () {
-
+        beaconAhoy = GetComponent<AudioSource>();
 		beacon = gameObject;
 		print (beacon);
 		player = GameObject.FindWithTag("Player");
@@ -22,17 +23,21 @@ public class BeaconCollection : MonoBehaviour {
 	void Update () {
 		Vector2 playerFlatVector = new Vector2 (player.transform.position.x, player.transform.position.z);
 		Vector2 beaconFlatVector = new Vector2 (beacon.transform.position.x, beacon.transform.position.z);
-		double beaconDistance = Vector2.Distance (playerFlatVector, beaconFlatVector);
+		float beaconDistance = Vector2.Distance (playerFlatVector, beaconFlatVector);
 		if (beaconDistance < BEACON_COLLECTION_DISTANCE) {
 			collectBeacon();
 		}
+        beaconAhoy.volume = 1f / Mathf.Clamp(beaconDistance, 1, 20);
+
 	}
 
 	void collectBeacon() {
 		GameManager.incrementCollectedBeacons();
-
+        beaconAhoy.Stop();
+        beaconPickUp.Play();
 		// Remove the beacon from the scene
-		GameObject.Destroy(beacon);
+        gameObject.transform.position = new Vector3(0f, 999f, 0f);
+		Destroy(beacon, beaconPickUp.clip.length);
 
 		print ("Beacon collected.");
 
