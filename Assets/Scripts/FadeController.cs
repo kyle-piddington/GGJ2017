8e103a2edd7d46fc553652a6df06d64eb5eeb003
GodScrub <david_avila_bhc@yahoo.com>
2017-01-22 09:39:28 -0800

    Pulse Visuals Updated UI Updated

---
+++ Assets/Scripts/FadeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeController : MonoBehaviour {
+    public Image fader;
+
+    private Color alpha;
+	// Use this for initialization
+	void Start () {
+        alpha = fader.color;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (alpha.a > 0f)
+            alpha.a -= .005f;
+        fader.color = alpha;
+	}
+}
