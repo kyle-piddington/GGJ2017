8e49fb65e054eb58de346165f020a4c76e330ea4
GodScrub <david_avila_bhc@yahoo.com>
2017-01-21 16:53:38 -0800

    Added SFX, Fixed Pulse Script Bugs

--- Assets/Scripts/PlayerPulseScript.cs
+++ Assets/Scripts/PlayerPulseScript.cs
@@ -2,10 +2,17 @@
 using System.Collections;
 
 public class PlayerPulseScript : MonoBehaviour {
+
     public float camShakeDampFactor = .25f;
     public float coolDownTime;
 	public float _pulseDist;
 	public float MaxCharge;
+
+    // Audio Cues
+    public SFXManager sfx;
+    public bool fullyCharged;
+    public bool charging;
+    public bool discharging;
 
     public float chargeRate = 10.0f;
     public float currentCharge = 0.0f;
@@ -14,7 +21,7 @@ public class PlayerPulseScript : MonoBehaviour {
 	public float chargeSpeed;
 	private float _sonarCharge;
     private bool canCharge = true;
-    private bool discharge = false;
+    
 	// Use this for initialization
 	void Start () {
 		
@@ -23,16 +30,19 @@ public class PlayerPulseScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        if (Input.GetKeyDown(KeyCode.Space) && canCharge)
+        if (Input.GetKey(KeyCode.Space) && canCharge)
         {
-            _sonarCharge = minSonar;
-        }
-        if (Input.GetKey(KeyCode.Space) && _sonarCharge < MaxCharge && canCharge)
-        {
-            _sonarCharge += Time.deltaTime * chargeRate;
+            charging = true;
+            discharging = false;
+
+            _sonarCharge += (Time.deltaTime * chargeRate) + minSonar;
             currentCharge = Mathf.Clamp(currentCharge + chargeRate * Time.deltaTime, 0, MaxCharge);
+
             if (currentCharge == MaxCharge)
             {
+                charging = false;
+                fullyCharged = true;
+                Debug.Log("Once");
                 canCharge = false;
             }
         }
@@ -42,13 +52,15 @@ public class PlayerPulseScript : MonoBehaviour {
             canCharge = false;
         }
       
-        if (discharge)
+        if (discharging)
         {
             currentCharge = Mathf.Clamp(currentCharge - Time.deltaTime * drainRate, 0, MaxCharge);
             if (currentCharge == 0)
             {
                 canCharge = true;
-                discharge = false;
+                discharging = false;
+                fullyCharged = false;
+                sfx.resetAudioMarkers();
             }              
         }    
 	}
@@ -60,7 +72,11 @@ public class PlayerPulseScript : MonoBehaviour {
 
     public void releaseCharge()
     {
+        charging = false;
+        discharging = true;
+
         StartCoroutine(Camera.main.GetComponent<CameraManager>().Shake((currentCharge / MaxCharge) * camShakeDampFactor));
+
         Debug.Log(_sonarCharge);
         Collider[] walls = Physics.OverlapSphere(transform.position, _sonarCharge + 5.0f);
         foreach (Collider c in walls)
@@ -73,6 +89,6 @@ public class PlayerPulseScript : MonoBehaviour {
         }
 
         _sonarCharge = 0;
-        discharge = true;
+        sfx.resetAudioMarkers();
     }
 }
