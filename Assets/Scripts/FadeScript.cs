5070d6f0cad023aba08ec94f8424fab446978d36
GodScrub <david_avila_bhc@yahoo.com>
2017-01-22 16:05:15 -0800

    Fixed conflict

---
+++ Assets/Scripts/FadeScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeScript : MonoBehaviour
+{
+    Text instructions;
+    public float fadeTime = 3;
+
+    private bool flicker = false;
+
+    IEnumerator fadeOut()
+    {
+        yield return new WaitForSeconds(fadeTime);
+        for(int i = 0; i < 9; i++)
+        {
+            instructions.enabled = flicker;
+            flicker = !flicker;
+            yield return new WaitForSeconds(1 / Mathf.Pow(2*(i + 1), 2));
+        }
+        
+        yield return null;
+    }
+    // Use this for initialization
+    void Start()
+    {
+        instructions = GetComponentInChildren<Text>();
+        StartCoroutine(fadeOut());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+}
